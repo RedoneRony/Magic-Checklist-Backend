7880fb57d214505dbd83fde38162cbae298e6dfc
MdRedone <redone.hassan@sjinnovation.com>
2023-01-06 00:14:44 +0600

    update on userother information controller file

--- controllers/userOtherInformationController.js
+++ controllers/userOtherInformationController.js
@@ -1,27 +1,46 @@
-import asyncHandler from "express-async-handler";
-import UserOtherInformation from "../models/userOtherInformationModel.js";
+import asyncHandler from 'express-async-handler'
+import UserOtherInformation from '../models/userOtherInformationModel.js'
 
 const getUserOtherInformation = asyncHandler(async (req, res) => {
-  const userOtherInfo = await UserOtherInformation.find({});
-  res.json(userOtherInfo);
-});
+  const userOtherInfo = await UserOtherInformation.find({})
+  res.json(userOtherInfo)
+})
 
-const createUserOtherInformation = asyncHandler(async (req, res) => {
+const createOrUpdateUserOtherInformation = asyncHandler(async (req, res) => {
   const userOtherInformation = await UserOtherInformation.findOne({
-    email: req.body.email,
-  }).exec();
-  const { email, agencyName, agencySize, location } = req.body;
+    email: req.body.email
+  }).exec()
+  const { email, agencyName, agencySize, location } = req.body
   if (userOtherInformation === null) {
     await UserOtherInformation.create({
       email,
       agencyName,
       agencySize,
-      location,
-    });
-    res.status(201).json({ message: "User Other Information Created" });
+      location
+    })
+    res.status(201).json({ message: 'User Other Information Created' })
   } else {
-    res.status(400).json({ message: "User Other Information already here" });
-  }
-});
+    UserOtherInformation.findOneAndUpdate(
+      { email: req.body.email },
+      {
+        $set: {
+          agencyName: agencyName,
+          agencySize: agencySize,
+          location: location
+        }
+      }
+    ).exec(function (err, result) {
+      if (err) {
+        console.log(err)
+        res.status(500).send(err)
+      } else {
+        res.status(200).send(result)
+      }
+    })
 
-export { getUserOtherInformation, createUserOtherInformation };
+    // await UserOtherInformation.findOneAndUpdate(filter, update)
+    // res.status(400).json({ message: ' Update User Other Information' })
+  }
+})
+
+export { getUserOtherInformation, createOrUpdateUserOtherInformation }
