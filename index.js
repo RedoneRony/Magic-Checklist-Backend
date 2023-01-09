d2d56d8393c82f4960408ddc1e02cf0efdb272e0
MdRedone <redone.hassan@sjinnovation.com>
2023-01-09 14:24:37 +0600

    add start,restart and stop command in package.json scripts section

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ if (process.env.NODE_ENV === "production") {
   );
 } else {
   app.get("/", (req, res) => {
-    res.send("API is running....");
+    res.send(" Tools23 API is running....");
   });
 }
 
