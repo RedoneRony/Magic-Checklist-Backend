e53288cea5bad2aa2d7620c3ec45df746a49afc9
MdRedone <redone.hassan@sjinnovation.com>
2023-01-09 18:27:58 +0600

    changes in index.js file

--- index.js
+++ index.js
@@ -23,17 +23,17 @@ app.use("/api/okr", okrRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/userOtherInformation", userOtherInformationRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "/frontend/build")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send(" Tools23 API is running....");
-  });
-}
+//   app.get("*", (req, res) =>
+//     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+//   );
+// } else {
+app.get("/", (req, res) => {
+  res.send(" Tools23 API is running....");
+});
+// }
 
 const Port = process.env.PORT || 8080;
 app.listen(Port, console.log("Listening buddy to port ", Port));
